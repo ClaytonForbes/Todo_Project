b0f4d7e7f6ef8e061181e459b6e2ceb560df733b
Suley890 <suleymanjama890@gmail.com>
2022-04-12 23:04:12 -0400

    Fixed Errors

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,122 +1,190 @@
-import React, {useLayoutEffect, useState} from 'react';
-import { StyleSheet, Text, View, TextInput,TouchableOpacity  } from 'react-native';// Use effect allows us to manage state of the component and any change in the componet
+import React, { useLayoutEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from "react-native"; // Use effect allows us to manage state of the component and any change in the componet
 //import {Ionicons} from "@expo/vector-icons"// use to give the options and the delete icon
 import Colors from "../constants/Colors";
-import Button from '../components/Button';
-import LabeledInput from '../components/LabeledInput';
+import Button from "../components/Button";
+import LabeledInput from "../components/LabeledInput";
 import validator from "validator";
-import {auth} from "firebase";
+import { auth } from "firebase";
+import * as ImagePicker from "expo-image-picker";
 
-
-
-
-
-//validates that the email and passwords with certain rules are met using the validator libaray 
-const validateFeilds = (email,password) =>{
-    const isValid = {
-        email:validator.isEmail(email),
-        password:validator.isStrongPassword(password, {
-            minLength:5,
-            minNumbers:1,
-            minUppercase:1,
-            minSymbols:1
-        })
-
-    }
-    return isValid
+//validates that the email and passwords with certain rules are met using the validator libaray
+const validateFeilds = (email, password) => {
+  const isValid = {
+    email: validator.isEmail(email),
+    password: validator.isStrongPassword(password, {
+      minLength: 5,
+      minNumbers: 1,
+      minUppercase: 1,
+      minSymbols: 1,
+    }),
+  };
+  return isValid;
 };
 const login = (email, password) => {
-    auth()
-        .signInWithEmailAndPassword(email, password)
-        .then(() => {
-            console.log("You are Logged in!");
-        });
+  auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(() => {
+      console.log("You are Logged in!");
+    });
 };
 
-const createAccount = (email,password) =>{
-    auth().createUserWithEmailAndPassword(email,password)// this returns a promise 
-    .then(({user})=>{
-        console.log("Creating new user.....");
+const createAccount = (email, password) => {
+  //   auth()
+  //     .createUserWithEmailAndPassword(email, password) // this returns a promise
+  //     .then(({ user }) => {
+  //       console.log("Creating new user.....");
+  //     });
+};
+
+export default () => {
+  const [image, setImage] = useState(null);
+  const [isCreateMode, setCreateMode] = useState(false); // enables/disables  the create or log in for page for users
+  const [emailField, setemailFeild] = useState({
+    text: "",
+    errorMessage: "",
+  });
+  const [passwordField, setPasswordFeild] = useState({
+    text: "",
+    errorMessage: "",
+  });
+  const [passwordReentryField, setPasswordReentryFeild] = useState({
+    text: "",
+    errorMessage: "",
+  });
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
     });
-}  
 
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>C_J_S_TODO</Text>
+      <View style={{ flex: 1 }}>
+        <LabeledInput
+          label="Email"
+          text={emailField.text}
+          onChangeText={(text) => {
+            setemailFeild({ text });
+          }}
+          errorMessage={emailField.errorMessage}
+          labelStyles={styles.label}
+          autoCompleteType={"email"}
+        />
 
+        <LabeledInput
+          label="Password"
+          text={passwordField.text}
+          onChangeText={(text) => {
+            setPasswordFeild({ text });
+          }}
+          secureTextEntry={true} //makes it so that the text data is password secure
+          errorMessage={passwordField.errorMessage}
+          labelStyles={styles.label}
+          autoCompleteType={"password"}
+        />
 
-export default () =>{
-    const [isCreateMode, setCreateMode] = useState(false);// enables/disables  the create or log in for page for users
-    const [emailField,setemailFeild] = useState({
-        text:"",
-        errorMessage:""
-    })
-    const [passwordField,setPasswordFeild] = useState({
-        text:"",
-        errorMessage:""
-    })
-    const [passwordReentryField,setPasswordReentryFeild] = useState({
-        text:"",
-        errorMessage:""
-    })
-    return <View style ={styles.container}>
-        <Text style ={styles.header}>C_J_S_TODO</Text>
-        <View style={{flex:1}}>
+        {isCreateMode && (
+          <LabeledInput
+            label="Re-enter Password"
+            text={passwordReentryField.text}
+            onChangeText={(text) => {
+              setPasswordReentryFeild({ text });
+            }}
+            secureTextEntry={true} //makes it so that the text data is password secure
+            errorMessage={passwordReentryField.errorMessage}
+            labelStyles={styles.label}
+          />
+        )}
 
-        <LabeledInput label="Email"text={emailField.text} onChangeText={(text) => {setemailFeild({text});}} 
-        errorMessage={emailField.errorMessage} labelStyles={styles.label}
-        autoCompleteType={"email"}/>
-
-        <LabeledInput label="Password"text={passwordField.text} onChangeText={(text) => {setPasswordFeild({text});}}
-        secureTextEntry={true} //makes it so that the text data is password secure
-        errorMessage={passwordField.errorMessage} labelStyles={styles.label}
-        autoCompleteType={"password"}/>
-
-        {isCreateMode &&(<LabeledInput label="Re-enter Password"text={passwordReentryField.text} onChangeText={(text) => {setPasswordReentryFeild({text});}} 
-        secureTextEntry={true} //makes it so that the text data is password secure
-        errorMessage={passwordReentryField.errorMessage} labelStyles={styles.label}
-        
-     />)}
-
-        
-     <TouchableOpacity onPress={() =>{setCreateMode(!isCreateMode)}}>
-            <Text style={{alignSelf:"center",color:Colors.blue,fontSize:16,margin:4}}>
-                {isCreateMode ? "You already have an account?": "Create new account"}
-            </Text>
+        <TouchableOpacity
+          onPress={() => {
+            setCreateMode(!isCreateMode);
+          }}
+        >
+          <Text
+            style={{
+              alignSelf: "center",
+              color: Colors.blue,
+              fontSize: 16,
+              margin: 4,
+            }}
+          >
+            {isCreateMode
+              ? "You already have an account?"
+              : "Create new account"}
+          </Text>
         </TouchableOpacity>
-        </View>
-      
-        <Button onPress={() => {
-            const isValid = validateFeilds(emailField.text,passwordField.text);
-            let isAllValid = true;
-            if(!isValid.email){
-                emailField.errorMessage = "Please enter a valid email";
-                setemailFeild({...emailField})// to not get rid of the text this is how it should be done.
-                isAllValid=false;
-            }
-            if (!isValid.password){
-                passwordField.errorMessage="Password must be atleast 5 long numbers, with uppercase, lowercase and a symbol character";
-                setPasswordFeild({...passwordField});
-                isAllValid = false;
-            }
+      </View>
 
-            if(isCreateMode && passwordReentryField.text != passwordField.text){
-                passwordReentryField.errorMessage="Passwords do not match"
-                setPasswordReentryFeild({...passwordReentryField});
-                isAllValid=false;
+      <Button
+        onPress={() => {
+          const isValid = validateFeilds(emailField.text, passwordField.text);
+          let isAllValid = true;
+          if (!isValid.email) {
+            emailField.errorMessage = "Please enter a valid email";
+            setemailFeild({ ...emailField }); // to not get rid of the text this is how it should be done.
+            isAllValid = false;
+          }
+          if (!isValid.password) {
+            passwordField.errorMessage =
+              "Password must be atleast 5 long numbers, with uppercase, lowercase and a symbol character";
+            setPasswordFeild({ ...passwordField });
+            isAllValid = false;
+          }
+
+          if (isCreateMode && passwordReentryField.text != passwordField.text) {
+            passwordReentryField.errorMessage = "Passwords do not match";
+            setPasswordReentryFeild({ ...passwordReentryField });
+            isAllValid = false;
+          }
+          if (isAllValid) {
+            isCreateMode
+              ? createAccount(emailField.text, passwordField.text)
+              : login(emailField.text, passwordField.text);
+          }
+        }}
+        buttonStyle={{ backgroundColor: Colors.red }}
+        text={isCreateMode ? "Create Account" : "Login"}
+      />
+
+      <View style={{ flex: 1, alignItems: "center" }}>
+        <TouchableOpacity onPress={pickImage} style={{ marginTop: 20 }}>
+          <Image
+            source={
+              image === null ? require("../assets/default.jpg") : { uri: image }
             }
-            if(isAllValid){
-                isCreateMode ? createAccount(emailField.text, passwordField.text): login();
-            }
-        }} buttonStyle={{backgroundColor: Colors.red }}text={isCreateMode ? "Create Account":"Login"}/>        
+            style={{ width: 200, height: 200 }}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        justifyContent: "space-between",
-        alignItems: "stretch",
-    },
-    label: { fontSize: 20, fontWeight: "bold", color: Colors.black },
-    header: { fontSize: 72, color: Colors.red, alignSelf: "center" },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "space-between",
+    alignItems: "stretch",
+  },
+  label: { fontSize: 20, fontWeight: "bold", color: Colors.black },
+  header: { fontSize: 50, color: Colors.red, alignSelf: "center" },
 });
--- Screens/TodoList.js
+++ Screens/TodoList.js
@@ -1,76 +1,87 @@
-import React, {useState, useLayoutEffect} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
-import {Ionicons} from "@expo/vector-icons"// use to give the options and the delete icon
+import React, { useState, useLayoutEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons"; // use to give the options and the delete icon
 import Colors from "../constants/Colors";
-import ToDoItem from '../components/ToDoItem';
-const renderAddListIcon= (addItem) => {
-    return(
-        <TouchableOpacity onPress={() => addItem({text: "", isChecked: false, isNewItem: true})}>
-            <Text style ={style.icon}>Add </Text>
-        </TouchableOpacity>
-    )
-}
+import ToDoItem from "../components/ToDoItem";
+const renderAddListIcon = (addItem) => {
+  return (
+    <TouchableOpacity
+      onPress={() => addItem({ text: "", isChecked: false, isNewItem: true })}
+    >
+      <Text style={style.icon}>Add </Text>
+    </TouchableOpacity>
+  );
+};
 
-export default({navigation}) => {
-    const [toDoItems, setToDoItems] = useState([{text: "hello", isChecked: false}])
-    const addItemToList = (item) => {
-        toDoItems.push(item);
-        setToDoItems(...[toDoItems]);
-    }
-    const removeItemFromLists = (index) =>{
-        toDoItems.splice(index , 1);
-        setToDoItems([...toDoItems])
+export default ({ navigation }) => {
+  const [toDoItems, setToDoItems] = useState([
+    { text: "hello", isChecked: false },
+  ]);
+  const addItemToList = (item) => {
+    toDoItems.push(item);
+    setToDoItems(...[toDoItems]);
+  };
+  const removeItemFromLists = (index) => {
+    toDoItems.splice(index, 1);
+    setToDoItems([...toDoItems]);
+  };
 
-    }
+  const updateItem = (index, item) => {
+    toDoItems[index] = item;
+    setToDoItems([...toDoItems]);
+  };
 
-    const updateItem = (index, item) => {
-        toDoItems[index] = item;
-        setToDoItems([...toDoItems])
-    }
-
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerRight:() => renderAddListIcon(addItemToList)
-        })
-    })
-    return(
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => renderAddListIcon(addItemToList),
+    });
+  });
+  return (
     <View style={style.container}>
-        <FlatList
+      <FlatList
         data={toDoItems}
-        renderItem={({item: {text, isChecked, isNewItem}, index}) => {
-            return <ToDoItem 
-            text={text} 
-            isChecked={isChecked} 
-            isNewItem={isNewItem}
-            onChecked={() => {
-                const toDoItem = toDoItems[index]
-                toDoItem.isChecked = !isChecked
-                updateItem(index, toDoItem)
-            }}
-            onChangeText={(newText) => {
-                const toDoItem = toDoItems[index]
+        renderItem={({ item: { text, isChecked, isNewItem }, index }) => {
+          return (
+            <ToDoItem
+              text={text}
+              isChecked={isChecked}
+              isNewItem={isNewItem}
+              onChecked={() => {
+                const toDoItem = toDoItems[index];
+                toDoItem.isChecked = !isChecked;
+                updateItem(index, toDoItem);
+              }}
+              onChangeText={(newText) => {
+                const toDoItem = toDoItems[index];
                 toDoItem.text = newText;
-                updateItem(index, toDoItem)
-            }}
-            onDelete={() => {
-                removeItemFromLists(index)
-            }}
+                updateItem(index, toDoItem);
+              }}
+              onDelete={() => {
+                removeItemFromLists(index);
+              }}
             />
+          );
         }}
-        />
-    </View>);
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </View>
+  );
+};
 
-    
-}
-
-const style =StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:"white",
-    },
-    icon:{
-        padding:5,
-        fontSize:32,
-        color:"white",
-    }
-})
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  icon: {
+    padding: 5,
+    fontSize: 32,
+    color: "white",
+  },
+});
